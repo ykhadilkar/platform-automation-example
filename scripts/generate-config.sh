d8fae70dc709204575e99ecaf1880bcbdfeb6fcd
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-10-19 10:30:45 -0400

    Updates generate config for new om cli version and updates to PKS 1.5.1

--- scripts/generate-config.sh
+++ scripts/generate-config.sh
@@ -21,7 +21,7 @@ slug=$(bosh interpolate ${versionfile} --path /pivnet-product-slug)
 
 tmpdir=environments/${iaas}/tile-configs/${product}-config
 mkdir -p ${tmpdir}
-om config-template --output-directory=${tmpdir} --pivnet-api-token ${PIVNET_TOKEN} --pivnet-product-slug  ${slug} --product-version ${version} --product-file-glob ${glob}
+om config-template --output-directory=${tmpdir} --pivnet-api-token ${PIVNET_TOKEN} --pivnet-product-slug  ${slug} --product-version ${version} --pivnet-file-glob ${glob}
 scrubbedversion=$(cut -d'-' -f1 <<< ${version})
 wrkdir=$(find ${tmpdir}/${product} -name "${scrubbedversion}*")
 if [ ! -f ${wrkdir}/product.yml ]; then
